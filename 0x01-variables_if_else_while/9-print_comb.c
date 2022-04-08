e249f98042b67b56c2137d3d1052b56acb355533
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-08 09:23:37 -0700

    commit dix

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+
+		if (a != 57;)
+		{
+			putchar(',');
+			putchar('\t');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

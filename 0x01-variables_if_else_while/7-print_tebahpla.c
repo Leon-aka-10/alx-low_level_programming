617774c1dec2eb7eb7d540a2802f568a86fb5706
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-08 08:35:19 -0700

    commit huit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets in lower case reversed.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int a;
+
+	for (a = 0; a = 26; a++)
+	{
+		putchar(alpha[a]);
+	}
+	putchar('\n');
+	return (0);
+}

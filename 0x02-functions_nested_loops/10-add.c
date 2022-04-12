26baf57639da6f1ad62d248433152d063083e7a9
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-12 11:35:22 -0700

    commit onze

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - sums two integers and returns
+ * the result.
+ *
+ * @m: number one.
+ *
+ * @n: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}

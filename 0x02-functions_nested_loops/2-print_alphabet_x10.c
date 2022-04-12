84e87f074d237019b4337d62c8e7e4b21e12bd4d
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-12 05:28:18 -0700

    commit trois

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int j, k;
+
+	for (j = 0; j <10; j++)
+	{
+		for (k = 97; k < 123; k++)
+		{
+			_putchar(k);
+		}
+	}
+	_putchar('\n');
+}

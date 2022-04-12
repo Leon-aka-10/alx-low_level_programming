d7ebc5130618dab4aee1ec7d5bc6d489c715cea2
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-12 12:42:13 -0700

    commit dix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,3 +5,28 @@
  * Return: no return
  */
 void times_table(void)
+{
+	int m, n, mul;
+
+	for (m = 0; m <= 9; m++)
+	{
+		_putchar(48);
+		for (n = 1; n <= 9; n++)
+		{
+			mul = m * n;
+			_putchar(44);
+			_putchar(32);
+			if (mul <= 9)
+			{
+				_putchar(32);
+				_putchar(mul + 48);
+			}
+			else
+			{
+				_putchar((mul / 10) + 48);
+				_putchar((mul % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}

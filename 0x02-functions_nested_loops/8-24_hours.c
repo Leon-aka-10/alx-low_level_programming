a00a57012b833a15a0bfac90a8aa9f0db46ad8ed
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-12 09:58:56 -0700

    commit neuf

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Return: no return
+ *
+ */
+void jack_bauer(void)
+{
+	int j, k, m, n;
+
+	for (j = 48; j <= 50; j++)
+	{
+		for (k = 48; k <= 57; k++)
+		{
+			for (m = 48; m <= 53; m++)
+			{
+				for (n = 48; n <= 57; n++)
+				{
+					if (j >= 50 && k >= 52)
+					{
+						break;
+					}
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}	
+			}
+		}
+	}
+}

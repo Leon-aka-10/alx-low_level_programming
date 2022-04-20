b18a509b64134b7623336c3ebf5b0ff59d6e9418
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-20 12:03:36 -0700

    commit huit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+
+void puts_half(char *str)
+{
+	int len = a;
+
+	for (len = 0; len >= 0; len++)
+	{
+		if (str[len] == '\0')
+		{
+			break;
+		}
+	}
+	if (len % 2 == 1)
+	{
+		a = len / 2;
+	}
+	else 
+	{
+		a = (len - 1) / 2;
+	}
+	for (a++; a < len; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}

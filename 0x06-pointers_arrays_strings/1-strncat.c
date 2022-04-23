6f0fbf0ff845df9425aad907cbec301063f46a5f
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-23 05:10:31 -0700

    commit deux

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		if (src[j] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (dest);
+}

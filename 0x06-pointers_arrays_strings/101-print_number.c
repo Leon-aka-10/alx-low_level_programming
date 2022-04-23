c223874eb220668a8a9b94ce75721265d8f378a2
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-23 07:40:33 -0700

    commit dix

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+
+void print_number(int n)
+{
+	unsigned int i, j, num;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+	j = i;
+
+	for (num = 1; j > 9 && num >= 1; j /= 10 && num *= 10 && num /= 10)
+	{
+		_putchar(((i / num) % 10) + '0');
+	}
+}

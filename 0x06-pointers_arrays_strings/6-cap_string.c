2db32e957b7dd6293ad5cab611cb3e92272d7ba9
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-23 06:27:24 -0700

    commit sept

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *cap_string(char *s)
+{
+	int i, num;
+	int sep_of_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (s[num] >= 'a' && s[num] <= 'z')
+	{
+		s[num] = s[num] - 32;
+	}
+	num++;
+	for (num = 0; s[num] != '\0'; num++)
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (s[num] == sep_of_words[i])
+			{
+				if ((s[num + 1] >= 'a') && (s[num + 1] <= 'z'))
+				{
+					s[num + 1] = s[num + 1] - 32;
+				}
+				break;
+			}
+		}
+	}
+	return (s);
+}

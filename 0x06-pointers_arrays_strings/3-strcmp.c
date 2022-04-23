fd08849e4f392b6ed130299f027b0f25166aa0d4
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-23 05:33:36 -0700

    commit quatre

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, j = 0;
 
-	for (; j == 0; i++)
+	for (; j == 0;)
 	{
 		if ((s1[i] == '\0') && (s2[i] == '\0'))
 		{
 			break;
 		}
 		j = s1[i] - s2[i];
+		i++;
 	}
 	return (j);
 }

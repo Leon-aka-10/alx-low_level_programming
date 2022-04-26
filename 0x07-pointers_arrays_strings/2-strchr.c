dd6ce7c2bf09ff85c8365853a661ef45fa1ec272
Leon-aka-10 <chuboy_10@yahoo.com>
2022-04-26 06:48:13 -0700

    commit trois

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,12 @@ char *_strchr(char *s, char c)
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
-		if (s[a] == 0)
+		if (s[a] == c)
 		{
 			return (s + a);
 		}
 	}
-	if (s[a] == 0)
+	if (s[a] == c)
 	{
 		return (s + a);
 	}

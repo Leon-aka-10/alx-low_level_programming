ea173d7b414e5e7df97e8917b0a5b4175ad33700
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-02 05:26:40 -0700

    commit cinq

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-
 /**
  * main - prints all arguments it receives.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-	int n;
+	int i;
 
-	for (n = 0; n < argc; n++)
-	{
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	}
 	return (0);
 }

bbc4ca7ec8e9218807c8768d3cf583c4eb1b9d18
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-09 06:12:00 -0700

    six

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,17 +14,18 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == old_size)
-		return (ptr);
-
-	if (!ptr)
-		ptr = malloc(new_size);
-
 	if (!new_size && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	if (!ptr)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
 	free(ptr);
 	ptr = malloc(new_size);
 

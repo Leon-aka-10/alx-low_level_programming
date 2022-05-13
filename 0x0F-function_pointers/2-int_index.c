d1586589b64d9b30d2cc42073654589fc569f356
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-13 06:04:18 -0700

    commit trois

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,15 +15,18 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a;
+
 	if (array && cmp)
 	{
 		if (size <= 0)
 			return (-1);
+
 		for (a = 0; a < size; a++)
 		{
 			if (cmp(*(array + a)))
 				return (a);
 		}
 	}
+
 	return (-1);
 }

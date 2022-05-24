a0738f55ef52e442aa2d4f3fa8b6757b9c711296
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-24 07:40:53 -0700

    commit un

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t elements = 0;
+
+	if (!h->str)
+		printf("[%d] %s\n", 0, "(nil)");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Aim is to improve ones understanding on data structures using singly linked lists

5035d946bd39a5dbc7682c18b80ab38c34130a9d
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-24 08:49:17 -0700

    commit trois

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	size_t nelements = 0;
+	ptr = *head;
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	while (str[nelements])
+	{
+		nelements++;
+	}
+	nelements = temp->len;
+	ptr = temp->next;
+	temp->next = temp;
+
+	return (ptr);
+}

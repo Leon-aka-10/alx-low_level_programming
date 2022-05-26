8177549c7e07d538dd5c02bb304b9f33e7fdc793
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-26 10:17:26 -0700

    commit six

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,12 +4,12 @@
  * pop_listint - deletes the head node of
  * a linked list
  * @head: head of a list.
+ *
  * Return: head node's data.
  */
 int pop_listint(listint_t **head)
 {
 	int headnode;
-	listint_t *h;
 	listint_t *temp;
 
 	if (*head == NULL)
@@ -19,11 +19,9 @@ int pop_listint(listint_t **head)
 
 	headnode = temp->n;
 
-	*h = temp->next;
+	*head = temp->next;
 
 	free(temp);
-
-	*head = h;
 
 	return (headnode);
 }

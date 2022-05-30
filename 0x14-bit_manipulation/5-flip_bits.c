2fbda27496d3074c26f14d0e261e087e4f26c326
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-30 13:07:40 -0700

    commit six

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int bits = 0;
 
-	for (; n ||m; n >>= 1, m >>= 1)
+	for (; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			bits++;

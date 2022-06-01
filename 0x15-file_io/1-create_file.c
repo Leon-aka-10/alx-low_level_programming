2de3c78bf538c58bb95919cf49161d2e232f74db
Leon-aka-10 <chuboy_10@yahoo.com>
2022-06-01 09:11:34 -0700

    commit deux

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int letters = 0;
+	int letters;
 	int wr;
 
 	if (filename == NULL)
@@ -24,6 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_context == NULL)
 		text_context = "";
 
+	letters = 0;
 	while (text_context[letters])
 		letters++;
 

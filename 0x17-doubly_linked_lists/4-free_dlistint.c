691cf5586cc27d60757ef11237ca89965d74d5a4
Leon-aka-10 <chuboy_10@yahoo.com>
2022-07-08 06:30:22 -0700

    commit deux

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* free_dlistint - frees a dlistint_t list
+*
+* @head: head of the list
+* Return: no return
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head)
+		while (head->prev)
+			head = head->prev;
+
+	while ((tmp = head))
+	{
+		head = head->next;
+		free(tmp);
+	}
+}

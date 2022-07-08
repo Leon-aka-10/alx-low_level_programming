ba70a0add8bb5b875c3fdecb3e07c1edcd7b6c21
Leon-aka-10 <chuboy_10@yahoo.com>
2022-07-08 06:34:48 -0700

    commit deux

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *h;
-        dlistint_t *new;
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
@@ -19,8 +19,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
-
-        h = *head;
+	h = *head;
 
 	if (h)
 	{
